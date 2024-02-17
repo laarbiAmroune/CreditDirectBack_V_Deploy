be4ec7e90ff92980bf98c9a918d12179eefcaf81
unknown <krimbelkacem.hamnache@fgei.ummto.dz>
2024-02-17 03:48:44 +0100

    nn

--- src/main/java/creditdirect/clientmicrocervice/config/CorsConfig.java
+++ src/main/java/creditdirect/clientmicrocervice/config/CorsConfig.java
@@ -5,21 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-
         config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*");// Use allowedOriginPattern instead of allowedOrigin
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*"); // Allow all origins
+        config.addAllowedHeader("*"); // Allow all headers
+        config.addAllowedMethod("*"); // Allow all methods
 
         source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
+
     }
+
 }
